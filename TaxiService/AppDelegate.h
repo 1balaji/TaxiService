7f7ea67565880314571f86bb2caa16beed52e05c
gonikaushal <gonikaushal@gmail.com>
2014-04-09 16:59:59 +0530

    Initial Commit

---
+++ TaxiService/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TaxiService
+//
+//  Created by chetan on 09/04/14.
+//  Copyright (c) 2014 chetan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TaxiService/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TaxiService
+//
+//  Created by chetan on 09/04/14.
+//  Copyright (c) 2014 chetan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
